8cf568a56459d2eb056795adb696e3e54e283b31
ielay <ielay@yandex.ru>
2018-08-04 17:39:10 +0300

    Check the method on a different number of matcher threads

--- src/main/java/proteisynchronization/task3/App.java
+++ src/main/java/proteisynchronization/task3/App.java
@@ -1,5 +1,8 @@
 package proteisynchronization.task3;
 
+import java.time.LocalDateTime;
+import java.util.Date;
+
 /**
  * @author lelay
  */
@@ -7,10 +10,14 @@ public class App {
     public static void main(String[] args) {
         LineMatcher matcher = new LineMatcher();
 
-        matcher.matchLine(5,
-                        "война",
-                        "src/main/resources/task3/testText1.txt",
-                        "src/main/resources/task3/outTestText1.txt",
-                        false);
+        for (int i = 1; i <= 10; ++i) {
+            long startTime = System.currentTimeMillis();
+            matcher.matchLine(i,
+                    "война",
+                    "src/main/resources/task3/testText1.txt",
+                    "src/main/resources/task3/outTestText1.txt",
+                    false);
+            System.out.println(System.currentTimeMillis() - startTime  + " Millis");
+        }
     }
 }
